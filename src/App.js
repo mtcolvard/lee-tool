c554772f18b89bc8c11c7f56e5287dea78497ac3
mtcolvard <matthew.colvard@gmail.com>
2021-09-12 19:27:24 +0200

    added fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import WhiteNoise from './WhiteNoise.js'
+import React from 'react'
+import Interface from './Interface'
 
-
-const App = () => {
-
-const examples = {
-  'WhiteNoise': <WhiteNoise/>
-}
-
-const example = location.hash.slice(1);
-
-const onExampleChange = e => {
-  location.hash = e.target.value;
-  location.reload();
-};
-
-  render(
-    (
-      <main>
-        <header>
-        <article>
-          <h1><strong>Noise Transmission Reduction Simulator</strong></h1>
-          <h2>Lee Brenner, Bay Area Noise Control</h2>
-        </article>
-          <label htmlFor="example-select">Select your scenario: </label>
-          <select id="example-select" onChange={onExampleChange} value={example}>
-          <option value="" disabled>Choose an example</option>
-            {
-              Object.keys(examples).map((ex, ei) => <option key={ei} value={ex}>{ex}</option>)
-            }
-          </select>
-        </header>
-        <hr/>
-        <span></span>
-        { examples[example] || null }
-      </main>
-    )
+function App() {
+  return (
+    <div className="divZero">
+      <Interface />
+    </div>
   )
 }
-export default App
 
-// <option value="" disabled>Choose an example</option>
+export default App
---
+++ src/Interface.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
+
+const Interface = () => {
+
+  const [play, setPlay] = useState(false)
+
+  return (
+    <div>
+      <div  className='playbutton' onClick={() => setPlay(true)}><FontAwesomeIcon icon={faPlay}/>
+      </div>
+      <div  className='stopButton' onClick={() => setPlay(false)}><FontAwesomeIcon icon={faStop}/>
+      </div>
+    </div>
+    )
+  }
+
+export default Interface
